d2b24a2b9f6e4956e5385c5efa4a7b780f3d7c1f
EranTimothy-dev <eranperera.dev@gmail.com>
2025-01-09 22:28:54 +0530

    added assertion methods

--- src/main/java/junittesting/Calculator.java
+++ src/main/java/junittesting/Calculator.java
@@ -6,4 +6,11 @@ public class Calculator {
         return a + b;
     }
 
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public boolean isEven(int i) {
+        return i % 2 == 0;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import junittesting.Calculator;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -12,5 +13,41 @@ public class CalculatorTest {
     public void testAddition(){
         int result = underTest.add(2,3);
         assertEquals(5, result);
+    }
+
+    @Test
+    public void division(){
+        int result = underTest.divide(6,2);
+        assertNotEquals(4,result);
+    }
+
+    @Test
+    public void isEven(){
+        boolean result = underTest.isEven(4);
+        assertTrue(result);
+    }
+
+    @Test
+    public void isEvenWithOddNumber(){
+        boolean result = underTest.isEven(5);
+        assertFalse(result);
+    }
+
+    @Test
+    public void divideByZero(){
+        Integer result = null;
+        try{
+            result = underTest.divide(6,0);
+        } catch (ArithmeticException e){
+            // Division by zero occurred
+        }
+        assertNull(result);
+    }
+
+    @Test
+    void divideNonZero(){
+        Integer result = underTest.divide(10,2);
+        assertNotNull(result);
+        assertEquals(5,result);
     }
 }
