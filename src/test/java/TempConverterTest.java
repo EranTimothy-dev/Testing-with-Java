36bdcd26a7a19377339cb186b22576fb24ea210d
EranTimothy-dev <eranperera.dev@gmail.com>
2025-01-25 00:02:17 +0530

    modified exception tests to check error message

--- src/test/java/TempConverterTest.java
+++ src/test/java/TempConverterTest.java
@@ -14,7 +14,8 @@ public class TempConverterTest {
 
     @Test
     public void fahrenheitToCelsius_celsiusTooLowTest(){
-        assertThrows(IllegalArgumentException.class,() -> TempConverter.fahrenheitToCelsius(-500));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,() -> TempConverter.fahrenheitToCelsius(-500));
+        assertEquals("Temperature cannot be below absolute zero (-459.67°F)", e.getMessage());
     }
 
     @Test
@@ -25,7 +26,8 @@ public class TempConverterTest {
 
     @Test
     public void celsiusToFahrenheit_celsiusTooLowTest(){
-        assertThrows(IllegalArgumentException.class, ()-> TempConverter.celsiusToFahrenheit(-300));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, ()-> TempConverter.celsiusToFahrenheit(-300));
+        assertEquals("Temperature cannot be below absolute zero (-273.15°C)", e.getMessage());
     }
 
 }
