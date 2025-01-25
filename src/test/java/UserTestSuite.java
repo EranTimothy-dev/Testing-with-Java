09a9a99d052e29050c42f169ed8ab68ee16890af
EranTimothy-dev <eranperera.dev@gmail.com>
2025-01-25 22:02:32 +0530

    creating test suites to group test related test cases

---
+++ src/test/java/UserTestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        UserRegistrationTests.class,
+        UserLoginTest.class
+})
+//@SelectPackages("com.package.testpackage")
+public class UserTestSuite {
+}
