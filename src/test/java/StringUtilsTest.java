f8f83c50ddeb0da74924aa03287597271cb30bd0
EranTimothy-dev <eranperera.dev@gmail.com>
2025-01-25 16:15:39 +0530

    Parameterized Tests with ValueSource

--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,34 +1,47 @@
 import junittesting.StringUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringUtilsTest {
 
-    @Test
-    void isPalindromeWithEmptyString() {
-        assertTrue(StringUtils.isPalindrome(""));
+    @ParameterizedTest
+    @ValueSource(strings = {"", "a", "racecar", "RaceCar"})
+    public void isPalidrome(String str){
+        assertTrue(StringUtils.isPalindrome(str));
     }
 
-    @Test
-    void isPalindromeWithSingleCharacter() {
-        assertTrue(StringUtils.isPalindrome("a"));
+    @ParameterizedTest
+    @ValueSource(strings = {"hello", "world", "java", "Junit"})
+    public void isNotPalindrome(String str){
+        assertFalse(StringUtils.isPalindrome(str));
     }
 
-    @Test
-    void isPalindromeWithPalindromeString() {
-        assertTrue(StringUtils.isPalindrome("racecar"));
-    }
-
-    @Test
-    void isPalindromeWithNonPalindromeString() {
-        assertFalse(StringUtils.isPalindrome("hello"));
-    }
-
-    @Test
-    void isPalindromeWithMixedCasePalindromeString() {
-        assertTrue(StringUtils.isPalindrome("RaceCar"));
-    }
+//    @Test
+//    void isPalindromeWithEmptyString() {
+//        assertTrue(StringUtils.isPalindrome(""));
+//    }
+//
+//    @Test
+//    void isPalindromeWithSingleCharacter() {
+//        assertTrue(StringUtils.isPalindrome("a"));
+//    }
+//
+//    @Test
+//    void isPalindromeWithPalindromeString() {
+//        assertTrue(StringUtils.isPalindrome("racecar"));
+//    }
+//
+//    @Test
+//    void isPalindromeWithNonPalindromeString() {
+//        assertFalse(StringUtils.isPalindrome("hello"));
+//    }
+//
+//    @Test
+//    void isPalindromeWithMixedCasePalindromeString() {
+//        assertTrue(StringUtils.isPalindrome("RaceCar"));
+//    }
 
 }
