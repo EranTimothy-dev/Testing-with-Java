be0686f892138a52ce5f47c96be8cf776ce440af
EranTimothy-dev <eranperera.dev@gmail.com>
2025-01-25 16:06:02 +0530

    Parameterized tests with EnumSource

--- src/test/java/UserRoleTest.java
+++ src/test/java/UserRoleTest.java
@@ -1,33 +1,51 @@
 import junittesting.UserRole;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserRoleTest {
 
-    @Test
-    void adminHasWritePermission() {
-        UserRole role = UserRole.ADMIN;
+    /**
+     * Using parameterized test with EnumSource to make tests more concise
+     * */
+
+    @ParameterizedTest
+    @EnumSource(value = UserRole.class, names = {"ADMIN", "MODERATOR"})
+    public void rolesWithWritePermission(UserRole role){
         assertTrue(role.hasWritePermission());
     }
 
-    @Test
-    void moderatorHasWritePermission() {
-        UserRole role = UserRole.MODERATOR;
-        assertTrue(role.hasWritePermission());
-    }
-
-    @Test
-    void userHasNoWritePermission() {
-        UserRole role = UserRole.USER;
+    @ParameterizedTest
+    @EnumSource(value = UserRole.class, names = {"USER", "GUEST"})
+    public void rolesWithoutWritePermission(UserRole role){
         assertFalse(role.hasWritePermission());
     }
 
-    @Test
-    void guestHasNoWritePermission() {
-        UserRole role = UserRole.GUEST;
-        assertFalse(role.hasWritePermission());
-    }
+//    @Test
+//    void adminHasWritePermission() {
+//        UserRole role = UserRole.ADMIN;
+//        assertTrue(role.hasWritePermission());
+//    }
+//
+//    @Test
+//    void moderatorHasWritePermission() {
+//        UserRole role = UserRole.MODERATOR;
+//        assertTrue(role.hasWritePermission());
+//    }
+//
+//    @Test
+//    void userHasNoWritePermission() {
+//        UserRole role = UserRole.USER;
+//        assertFalse(role.hasWritePermission());
+//    }
+//
+//    @Test
+//    void guestHasNoWritePermission() {
+//        UserRole role = UserRole.GUEST;
+//        assertFalse(role.hasWritePermission());
+//    }
 
 }
